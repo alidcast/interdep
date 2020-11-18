c9cf3e4980300a2c64b1bb65827dc5f6d9d4b333
alidlorenzo <alidcastano@gmail.com>
2020-11-17 22:41:42 -0600

    cleanup unused fns

--- src/interdep/deps/util.clj
+++ src/interdep/deps/util.clj
@@ -5,27 +5,12 @@
    [clojure.java.io :as io]
    [clojure.edn :as edn]))
 
-(def ^:private cfg
-  {:build-dir ".repo/main"
-   :deps-file "deps.edn"})
-
 (defn join-path
   "Join path strings separated by single forward slash."
   [& paths]
   (str/replace
    (apply str (interpose "/" paths))
    #"\/\/" "/"))
-
-(defn- join-path-keys
-  "Joins paths of key-values in passed map.
-   Makes it easy to join config paths."
-  [m & keys]
-  (apply join-path (mapv #(m %) keys)))
-
-(defn deps-build-path
-  "Get deps build path."
-  []
-  (join-path-keys cfg :build-dir :deps-file))
 
 (defn read-root-deps
   "Get root deps cofig loaded by clojure cli."
@@ -36,6 +21,3 @@
   "Read a sub-directory's deps config."
   [dir]
   (-> (str dir "/deps.edn") io/file slurp edn/read-string))
-
-(comment 
-  (-> (System/getProperty "clojure.basis") io/file slurp edn/read-string))
